33911b480ccd04eee852639522462403a56361b0
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2021-09-30 22:42:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
-var filled = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
-function func(name) {
-    document.getElementById(name).src = "./images/x.jpg";
-    pushtoarray(name);
-    myturn();
-}
-function pushtoarray(n) {
+var filled = ["00", "01", "02", "10", "11", "12", "20", "21", "22"];
+var userSelector = [[" "," "," "],
+                    [" "," "," "],
+                    [" "," "," "] 
+                   ];
+var machineSelector = [[" "," "," "],
+                       [" "," "," "],
+                       [" "," "," "]
+                     ];
+
+function popElement(n) {
     filled = filled.filter(item => item !== n);
 }
 
-function myturn() {
+function RandomSelector() {
     var sel = filled[Math.floor(Math.random() * filled.length)];
-    filled = filled.filter(item => item !== sel);
+    popElement(sel);
     document.getElementById(sel).src = "./images/o.png";
+    machineSelector[sel[0]][sel[1]] = "X";
 }
 
-function func(row, col) {
-    alert(row + " " + col);
+function func(row, col, id) {
+    document.getElementById(id).src = "./images/x.jpg";
+    userSelector[id[0]][id[1]] = "O";
+    popElement(id);
+    RandomSelector();
+    console.log(userSelector);
+}
+
+function verifywin(){
+    var diganols = 0,revdiganols=0;
+    for(var i=0;i<3;i++){
+        for(var j=0;j<3;j++){
+            if(i == j){
+                if(userSelector[i][j]!=" ")
+                diganols++;
+            }
+            if(i+j == 2){
+                if(userSelector[i][j]!=" ")
+                revdiganols++;
+            }
+        }
+    }
+    if(diganols == 3 || revdiganols == 3){
+        console.log("user winns");
+    }
+    else{
+        console.log("not a winner");
+    }
 }
