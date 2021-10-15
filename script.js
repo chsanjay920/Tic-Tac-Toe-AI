2b88e7d4456f5f50ef01bdab4cf38edff82c3be2
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2021-10-15 18:21:07 +0530

    2d array added

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 var filled = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
-var filledarray = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+// var filledarray = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+var board = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
+];
+
+var dic ={
+    "0":"00",
+    "1":"01",
+    "2":"02",
+    "3":"10",
+    "4":"11",
+    "5":"12",
+    "6":"20",
+    "7":"21",
+    "8":"22",
+}
 
 window.onload = function () {
     RandomSelector();
@@ -11,50 +28,55 @@ function RandomSelector() {
     var sel = filled[Math.floor(Math.random() * filled.length)];
     popElement(sel);
     document.getElementById(sel).src = "https://raw.githubusercontent.com/chsanjay920/Tic-Tac-Toe-AI/main/images/circle.png";
-    filledarray[sel] = "O";
+    // filledarray[sel] = "O";
+    board[dic[sel][0]][dic[sel][1]] = "O";
 }
 function getindex(index) {
     document.getElementById(index).src = "https://raw.githubusercontent.com/chsanjay920/Tic-Tac-Toe-AI/main/images/machine%20(1).png";
     popElement(index);
-    filledarray[index] = "X";
+    // filledarray[index] = "X";
+    board[dic[index][0]][dic[index][1]] = "X";
     RandomSelector();
     var flag = verfiwinner();
     if(flag == true){
         location.reload();
     }
+    console.log(board);
 }
 function verfiwinner(){
     //verify rows
     var f = 0;
-    for(var i = 0;i < 9;i++){
-        if(filledarray[i] == " "){
-            f = 1;
-            break;
+    for(var i = 0;i < 3;i++){
+        for(var j=0;j<3;j++){
+            if(board[i][j] == " "){
+                f = 1;
+                break;
+            }
         }
     }
-    if(filledarray[0] == filledarray[1] && filledarray[1] == filledarray[2] && (filledarray[0] === "X" || filledarray[0] === "O")){
-        poppingwinner(filledarray[0]+" won the match");
+    if(board[0][0] == board[0][1] && board[0][1] == board[0][2] && (board[0][0] === "X" || board[0][0] === "O")){
+        poppingwinner(board[0][0]+" won the match");
     }
-    else if(filledarray[3] == filledarray[4] && filledarray[4] == filledarray[5] && (filledarray[3] === "X" || filledarray[3] === "O")){
-        poppingwinner(filledarray[3]+" won the match");
+    else if(board[1][0] == board[1][1] && board[1][1] == board[1][2] && (board[1][0] === "X" || board[1][0] === "O")){
+        poppingwinner(board[1][0]+" won the match");
     }
-    else if(filledarray[6] == filledarray[7] && filledarray[7] == filledarray[8] && (filledarray[6] === "X" || filledarray[6] === "O")){
-        poppingwinner(filledarray[6]+" won the match");
+    else if(board[2][0] == board[2][1] && board[2][1] == board[2][2] && (board[2][0] === "X" || board[2][0] === "O")){
+        poppingwinner(board[2][0]+" won the match");
     }
-    else if(filledarray[0] == filledarray[3] && filledarray[3] == filledarray[6] && (filledarray[0] === "X" || filledarray[0] === "O")){
-        poppingwinner(filledarray[0]+" won the match");
+    else if(board[0][0] == board[1][0] && board[1][0] == board[2][0] && (board[0][0] === "X" || board[0][0] === "O")){
+        poppingwinner(board[0][0]+" won the match");
     }
-    else if(filledarray[1] == filledarray[4] && filledarray[4] == filledarray[7] && (filledarray[1] === "X" || filledarray[1] === "O")){
-        poppingwinner(filledarray[1]+" won the match");
+    else if(board[0][1] == board[1][1] && board[1][1] == board[2][1] && (board[0][1] === "X" || board[0][1] === "O")){
+        poppingwinner(board[0][1]+" won the match");
     }
-    else if(filledarray[2] == filledarray[5] && filledarray[5] == filledarray[8] && (filledarray[2] === "X" || filledarray[2] === "O")){
-        poppingwinner(filledarray[2]+" won the match");
+    else if(board[0][2] == board[1][2] && board[1][2] == board[2][2] && (board[0][2] === "X" || board[0][2] === "O")){
+        poppingwinner(board[0][2]+" won the match");
     }
-    else if(filledarray[0] == filledarray[4] && filledarray[4] == filledarray[8] && (filledarray[0] === "X" || filledarray[0] === "O")){
-        poppingwinner(filledarray[0]+" won the match");
+    else if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && (board[0][0] === "X" || board[0][0] === "O")){
+        poppingwinner(board[0][0]+" won the match");
     }
-    else if(filledarray[6] == filledarray[4] && filledarray[4] == filledarray[2] && (filledarray[6] === "X" || filledarray[6] === "O")){
-        poppingwinner(filledarray[6]+" won the match");
+    else if(board[2][0] == board[1][1] && board[1][1] == board[0][2] && (board[2][0] === "X" || board[2][0] === "O")){
+        poppingwinner(board[2][0]+" won the match");
     }
     else if(f === 0){
         poppingwinner("draw match");
