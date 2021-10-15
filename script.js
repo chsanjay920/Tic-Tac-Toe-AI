d257dd1703cf725fd91bd3db7fac0ed48d8437ba
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2021-10-15 16:24:13 +0530

    another approach to verify
    simple approach added to validate the tic-tak-toe

--- script.js
+++ script.js
@@ -1,76 +1,71 @@
-var filled = ["00", "01", "02", "10", "11", "12", "20", "21", "22"];
-var userSelector = [[" ", " ", " "],
-[" ", " ", " "],
-[" ", " ", " "]
-];
+var filled = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+var filledarray = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+
 window.onload = function () {
     RandomSelector();
 }
 function popElement(n) {
     filled = filled.filter(item => item !== n);
 }
-
 function RandomSelector() {
     var sel = filled[Math.floor(Math.random() * filled.length)];
     popElement(sel);
     document.getElementById(sel).src = "https://raw.githubusercontent.com/chsanjay920/Tic-Tac-Toe-AI/main/images/circle.png";
-    userSelector[sel[0]][sel[1]] = "X";
+    filledarray[sel] = "O";
 }
-
-function func(row, col, id) {
-    document.getElementById(id).src = "https://raw.githubusercontent.com/chsanjay920/Tic-Tac-Toe-AI/main/images/machine%20(1).png";
-    userSelector[id[0]][id[1]] = "O";
-    popElement(id);
+function getindex(index) {
+    document.getElementById(index).src = "https://raw.githubusercontent.com/chsanjay920/Tic-Tac-Toe-AI/main/images/machine%20(1).png";
+    popElement(index);
+    filledarray[index] = "X";
     RandomSelector();
-    console.log(userSelector);
-    verifywin()
+    var flag = verfiwinner();
+    if(flag == true){
+        location.reload();
+    }
 }
-
-function verifywin() {
-    var diganolsofuser = 0, revdiganolsofuser = 0, diganols = 0, revdiganols = 0;
-    for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-            if (i == j) {
-                if (userSelector[i][j] == "O")
-                    diganolsofuser++;
-                if (userSelector[i][j] == "X")
-                    diganols++;
-            }
-            if (i + j == 2) {
-                if (userSelector[i][j] == "O")
-                    revdiganolsofuser++;
-                if (userSelector[i][j] == "X")
-                    revdiganols++;
-            }
+function verfiwinner(){
+    //verify rows
+    var f = 0;
+    for(var i = 0;i < 9;i++){
+        if(filledarray[i] == " "){
+            f = 1;
+            break;
         }
     }
-    if (diganolsofuser == 3 || revdiganolsofuser == 3 || (
-        (userSelector[0][0] == "O" && userSelector[0][1] == "O" && userSelector[0][2] == "O") ||
-        (userSelector[1][0] == "O" && userSelector[1][1] == "O" && userSelector[1][2] == "O") ||
-        (userSelector[2][0] == "O" && userSelector[2][1] == "O" && userSelector[2][2] == "O") ||
-        (userSelector[0][0] == "O" && userSelector[1][0] == "O" && userSelector[2][0] == "O") ||
-        (userSelector[0][1] == "O" && userSelector[1][1] == "O" && userSelector[2][1] == "O") ||
-        (userSelector[0][2] == "O" && userSelector[1][2] == "O" && userSelector[2][2] == "O")
-    )) {
-        console.log("user winns WON THE GAME");
-        // alert("you won");
-        winner("YOU WON THE GAME");
+    if(filledarray[0] == filledarray[1] && filledarray[1] == filledarray[2] && (filledarray[0] === "X" || filledarray[0] === "O")){
+        poppingwinner(filledarray[0]+" won the match");
     }
-    else if (diganols == 3 || revdiganols == 3 || (
-        (userSelector[0][0] == "X" && userSelector[0][1] == "X" && userSelector[0][2] == "X") ||
-        (userSelector[1][0] == "X" && userSelector[1][1] == "X" && userSelector[1][2] == "X") ||
-        (userSelector[2][0] == "X" && userSelector[2][1] == "X" && userSelector[2][2] == "X") ||
-        (userSelector[0][0] == "X" && userSelector[1][0] == "X" && userSelector[2][0] == "X") ||
-        (userSelector[0][1] == "X" && userSelector[1][1] == "X" && userSelector[2][1] == "X") ||
-        (userSelector[0][2] == "X" && userSelector[1][2] == "X" && userSelector[2][2] == "X")
-    )) {
-        console.log("machine winns");
-        // alert("machine won");
-        winner("MACHINE WON THE GAME");
+    else if(filledarray[3] == filledarray[4] && filledarray[4] == filledarray[5] && (filledarray[3] === "X" || filledarray[3] === "O")){
+        poppingwinner(filledarray[3]+" won the match");
+    }
+    else if(filledarray[6] == filledarray[7] && filledarray[7] == filledarray[8] && (filledarray[6] === "X" || filledarray[6] === "O")){
+        poppingwinner(filledarray[6]+" won the match");
+    }
+    else if(filledarray[0] == filledarray[3] && filledarray[3] == filledarray[6] && (filledarray[0] === "X" || filledarray[0] === "O")){
+        poppingwinner(filledarray[0]+" won the match");
+    }
+    else if(filledarray[1] == filledarray[4] && filledarray[4] == filledarray[7] && (filledarray[1] === "X" || filledarray[1] === "O")){
+        poppingwinner(filledarray[1]+" won the match");
+    }
+    else if(filledarray[2] == filledarray[5] && filledarray[5] == filledarray[8] && (filledarray[2] === "X" || filledarray[2] === "O")){
+        poppingwinner(filledarray[2]+" won the match");
+    }
+    else if(filledarray[0] == filledarray[4] && filledarray[4] == filledarray[8] && (filledarray[0] === "X" || filledarray[0] === "O")){
+        poppingwinner(filledarray[0]+" won the match");
+    }
+    else if(filledarray[6] == filledarray[4] && filledarray[4] == filledarray[2] && (filledarray[6] === "X" || filledarray[6] === "O")){
+        poppingwinner(filledarray[6]+" won the match");
+    }
+    else if(f === 0){
+        poppingwinner("draw match");
+    }
+    else{
+        return false;
     }
 }
 
-function winner(name) {
+
+function poppingwinner(name) {
     var modal = document.getElementById("popup");
     var span = document.getElementsByClassName("close")[0];
     document.getElementById('Winner').innerHTML = name;
